f59e47af8d70458e7b314dad727ce2b0e90576c5
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2023-12-25 09:08:11 +0200

    method windows handles & switch to window

--- src/test/java/methods/ClickSelenium.java
+++ src/test/java/methods/ClickSelenium.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.Set;
+
 public class ClickSelenium {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("file://D:\\testing\\Automation project\\Qacart_selenium_course\\src\\main\\resources\\index.html");
-         driver.manage().window().maximize();
-        driver.manage().window().wait(3000,1000);
+        driver.manage().window().maximize();
+        //driver.get("https://mydexefcustomerapi.azurewebsites.net/swagger/index.html");
+        //driver.get("http://9.0.0.45:11080/swagger/index.html");
+
 
                        /// DropDown //
         /*
@@ -23,9 +26,37 @@ public class ClickSelenium {
          */
                                                 /// is Displayed //
 
+      //////////////                              getWindowHandle
+/*
+        driver.get("https://qacart.com");
+         String parent = driver.getWindowHandle();
+        System.out.println(driver.getWindowHandles());
+          */
+                                   //// getWindowHandles //
+    /*
+     driver.get("https://the-internet.herokuapp.com/login");
+            System.out.println(driver.getWindowHandle());
+        driver.findElement(By.linkText("Elemental Selenium")).click();
+            System.out.println(driver.getWindowHandles());
+*/
+                                   //// switch to window //
+
+        driver.get("https://the-internet.herokuapp.com/login");
+       String parent = driver.getWindowHandle();
+        driver.findElement(By.linkText("Elemental Selenium")).click();
+        Set<String> allwindows = driver.getWindowHandles();
+        System.out.println(allwindows);
+
+        for(String window:allwindows){
+            if (!window.equalsIgnoreCase(parent)){
+              System.out.println(driver.getTitle());
+             driver.switchTo().window(window);
+                System.out.println(driver.getTitle());
+            }
+        }
 
 
-        driver.quit();
+//        driver.quit();
 
     }
 }
