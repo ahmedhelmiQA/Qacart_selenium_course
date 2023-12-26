5ba952afdbd19564552fb233fc6178e05b954207
ahmedhelmiQA <ahmed.systemtester@gmail.com>
2023-12-26 13:03:06 +0200

    implicitlyWait

--- src/test/java/methods/Waits.java
+++ src/test/java/methods/Waits.java
@@ -3,14 +3,35 @@ package methods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Waits {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("file:///D:/testing/Automation project/Qacart_selenium_course/HTML/wait.html");
+        //                          sleep
+        /*
         Thread.sleep(6000);
         driver.findElement(By.className("primary")).click();
+        */
+         //                          implicitlyWait
+   /*
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.className("primary")).click();
+   */
+        //                           wait an Element to be visible
+        /*
+        new WebDriverWait(driver,Duration.ofSeconds(10))
+        .until(ExpectedConditions.visibilityOfElementLocated(By.className("primary"))).click();
+        */
+
+
+
 
     }
 }
